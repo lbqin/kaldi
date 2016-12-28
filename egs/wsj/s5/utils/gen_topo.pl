3e6435681d676fda8b743ddb1a69281b87657e6c
vsooda <liushouda@qq.com>
2016-12-28 12:06:05 +0800

    change default hmm topo and split scp for tts

--- egs/wsj/s5/utils/gen_topo.pl
+++ egs/wsj/s5/utils/gen_topo.pl
@@ -34,46 +34,18 @@ for ($state = 0; $state < $num_nonsil_states; $state++) {
 }
 print "<State> $num_nonsil_states </State>\n"; # non-emitting final state.
 print "</TopologyEntry>\n";
-# Now silence phones.  They have a different topology-- apart from the first and
-# last states, it's fully connected, as long as you have >= 3 states.
 
-if ($num_sil_states > 1) {
-  $transp = 1.0 / ($num_sil_states-1);
-  print "<TopologyEntry>\n";
-  print "<ForPhones>\n";
-  print "$sil_phones\n";
-  print "</ForPhones>\n";
-  print "<State> 0 <PdfClass> 0 ";
-  for ($nextstate = 0; $nextstate < $num_sil_states-1; $nextstate++) { # Transitions to all but last
-    # emitting state.
-    print "<Transition> $nextstate $transp ";
-  }
-  print "</State>\n";
-  for ($state = 1; $state < $num_sil_states-1; $state++) { # the central states all have transitions to
-    # themselves and to the last emitting state.
-    print "<State> $state <PdfClass> $state ";
-    for ($nextstate = 1; $nextstate < $num_sil_states; $nextstate++) {
-      print "<Transition> $nextstate $transp ";
-    }
-    print "</State>\n";
-  }
-  # Final emitting state (non-skippable).
-  $state = $num_sil_states-1;
-  print "<State> $state <PdfClass> $state <Transition> $state 0.75 <Transition> $num_sil_states 0.25 </State>\n";
-  # Final nonemitting state:
-  print "<State> $num_sil_states </State>\n";
-  print "</TopologyEntry>\n";
-} else {
-  print "<TopologyEntry>\n";
-  print "<ForPhones>\n";
-  print "$sil_phones\n";
-  print "</ForPhones>\n";
-  print "<State> 0 <PdfClass> 0 ";
-  print "<Transition> 0 0.75 ";
-  print "<Transition> 1 0.25 ";
-  print "</State>\n";
-  print "<State> $num_nonsil_states </State>\n"; # non-emitting final state.
-  print "</TopologyEntry>\n";
+# use the same topology as non silence phone
+
+print "<TopologyEntry>\n";
+print "<ForPhones>\n";
+print "$sil_phones\n";
+print "</ForPhones>\n";
+for ($state = 0; $state < $num_sil_states; $state++) {
+  $statep1 = $state+1;
+  print "<State> $state <PdfClass> $state <Transition> $state 0.75 <Transition> $statep1 0.25 </State>\n";
 }
+print "<State> $num_sil_states </State>\n"; # non-emitting final state.
+print "</TopologyEntry>\n";
 
 print "</Topology>\n";
